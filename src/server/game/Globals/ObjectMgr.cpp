SunstriderEmu/sunstrider-legacy

commit 27d03800b0d40ea8f0b2d64c8b6a340c0c791a4e

Some renames and arguments changes to match TC on Vendor item lists
Former-commit-id: 3a7743c812cd393b6a40310570662f6042ea8533

src/server/game/Globals/ObjectMgr.cpp
@@ -7684,7 +7684,7 @@ void ObjectMgr::LoadVendors()
 
         VendorItemData& vList = m_mCacheVendorItemMap[entry];
 
-        vList.AddItem(proto,maxcount,incrtime,ExtendedCost);
+        vList.AddItem(item_id, maxcount, incrtime, ExtendedCost);
         ++count;
 
     } while (result->NextRow());
@@ -7915,7 +7915,7 @@ void ObjectMgr::LoadGossipMenuItems()
 void ObjectMgr::AddVendorItem( uint32 entry, ItemTemplate const *proto, uint32 maxcount, uint32 incrtime, uint32 extendedcost, bool savetodb)
 {
     VendorItemData& vList = m_mCacheVendorItemMap[entry];
-    vList.AddItem(proto,maxcount,incrtime,extendedcost);
+    vList.AddItem(entry, maxcount, incrtime, extendedcost);
 
     if(savetodb) 
         WorldDatabase.PExecute("INSERT INTO npc_vendor (entry,item,maxcount,incrtime,extendedcost) VALUES('%u','%u','%u','%u','%u')",entry, proto->ItemId, maxcount,incrtime,extendedcost);

src/server/game/CMakeLists.txt
@@ -1,7 +1,7 @@
 IF(NOT PLAYERBOT)
     SET(EXCLUDE_PLAYERBOT ${CMAKE_CURRENT_SOURCE_DIR}/playerbot)
 ENDIF(NOT PLAYERBOT)
-      
+     
 IF(NOT TESTS)
 	SET(EXCLUDE_TESTS ${CMAKE_CURRENT_SOURCE_DIR}/testing)
 ENDIF(NOT TESTS)

src/server/game/Entities/Creature/Creature.cpp
@@ -77,7 +77,7 @@ bool VendorItemData::RemoveItem( uint32 item_id )
 {
     for(auto i = m_items.begin(); i != m_items.end(); ++i )
     {
-        if((*i)->proto->ItemId==item_id)
+        if ((*i)->item == item_id)
         {
             m_items.erase(i);
             return true;
@@ -89,7 +89,7 @@ bool VendorItemData::RemoveItem( uint32 item_id )
 size_t VendorItemData::FindItemSlot(uint32 item_id) const
 {
     for(size_t i = 0; i < m_items.size(); ++i )
-        if(m_items[i]->proto->ItemId==item_id)
+        if(m_items[i]->item == item_id)
             return i;
     return m_items.size();
 }
@@ -97,7 +97,7 @@ size_t VendorItemData::FindItemSlot(uint32 item_id) const
 VendorItem const* VendorItemData::FindItem(uint32 item_id) const
 {
     for(auto m_item : m_items)
-        if(m_item->proto->ItemId==item_id)
+        if(m_item->item == item_id)
             return m_item;
     return nullptr;
 }
@@ -3032,7 +3032,7 @@ uint32 Creature::GetVendorItemCurrentCount(VendorItem const* vItem)
 
     auto itr = m_vendorItemCounts.begin();
     for(; itr != m_vendorItemCounts.end(); ++itr)
-        if(itr->itemId==vItem->proto->ItemId)
+        if (itr->itemId == vItem->item)
             break;
 
     if(itr == m_vendorItemCounts.end())
@@ -3044,7 +3044,7 @@ uint32 Creature::GetVendorItemCurrentCount(VendorItem const* vItem)
 
     if( vCount->lastIncrementTime + vItem->incrtime <= ptime )
     {
-        ItemTemplate const* pProto = sObjectMgr->GetItemTemplate(vItem->proto->ItemId);
+        ItemTemplate const* pProto = sObjectMgr->GetItemTemplate(vItem->item);
 
         uint32 diff = uint32((ptime - vCount->lastIncrementTime)/vItem->incrtime);
         if((vCount->count + diff * pProto->BuyCount) >= vItem->maxcount )
@@ -3067,13 +3067,13 @@ uint32 Creature::UpdateVendorItemCurrentCount(VendorItem const* vItem, uint32 us
 
     auto itr = m_vendorItemCounts.begin();
     for(; itr != m_vendorItemCounts.end(); ++itr)
-        if(itr->itemId==vItem->proto->ItemId)
+        if(itr->itemId==vItem->item)
             break;
 
     if(itr == m_vendorItemCounts.end())
     {
         uint32 new_count = vItem->maxcount > used_count ? vItem->maxcount-used_count : 0;
-        m_vendorItemCounts.push_back(VendorItemCount(vItem->proto->ItemId,new_count));
+        m_vendorItemCounts.push_back(VendorItemCount(vItem->item,new_count));
         return new_count;
     }
 
@@ -3082,13 +3082,14 @@ uint32 Creature::UpdateVendorItemCurrentCount(VendorItem const* vItem, uint32 us
     time_t ptime = time(nullptr);
 
     if( vCount->lastIncrementTime + vItem->incrtime <= ptime )
-    {
-        uint32 diff = uint32((ptime - vCount->lastIncrementTime)/vItem->incrtime);
-        if((vCount->count + diff * vItem->proto->BuyCount) < vItem->maxcount )
-            vCount->count += diff * vItem->proto->BuyCount;
-        else
-            vCount->count = vItem->maxcount;
-    }
+        if (ItemTemplate const* pProto = sObjectMgr->GetItemTemplate(vItem->item))
+        {
+            uint32 diff = uint32((ptime - vCount->lastIncrementTime)/vItem->incrtime);
+            if((vCount->count + diff * pProto->BuyCount) < vItem->maxcount )
+                vCount->count += diff * pProto->BuyCount;
+            else
+                vCount->count = vItem->maxcount;
+        }
 
     vCount->count = vCount->count > used_count ? vCount->count-used_count : 0;
     vCount->lastIncrementTime = ptime;

src/server/game/Entities/Creature/Creature.h
@@ -425,10 +425,10 @@ enum ChatType
 // Vendors
 struct VendorItem
 {
-    VendorItem(ItemTemplate const* proto, uint32 _maxcount, uint32 _incrtime, uint32 _ExtendedCost)
-        : proto(proto), maxcount(_maxcount), incrtime(_incrtime), ExtendedCost(_ExtendedCost) {}
+    VendorItem(uint32 _item, uint32 _maxcount, uint32 _incrtime, uint32 _ExtendedCost)
+        : item(_item), maxcount(_maxcount), incrtime(_incrtime), ExtendedCost(_ExtendedCost) {}
 
-    ItemTemplate const* proto;
+    uint32 item;
     uint32 maxcount;                                        // 0 for infinity item amount
     uint32 incrtime;                                        // time for restore items amount if maxcount != 0
     uint32 ExtendedCost;
@@ -446,9 +446,9 @@ struct VendorItemData
     }
     bool Empty() const { return m_items.empty(); }
     uint8 GetItemCount() const { return m_items.size(); }
-    void AddItem( ItemTemplate const *proto, uint32 maxcount, uint32 ptime, uint32 ExtendedCost)
+    void AddItem(uint32 item, uint32 maxcount, uint32 ptime, uint32 ExtendedCost)
     {
-        m_items.push_back(new VendorItem(proto, maxcount, ptime, ExtendedCost));
+        m_items.push_back(new VendorItem(item, maxcount, ptime, ExtendedCost));
     }
     bool RemoveItem( uint32 item_id );
     VendorItem const* FindItem(uint32 item_id) const;

src/server/game/Entities/Player/Player.cpp
@@ -19167,7 +19167,7 @@ void Player::InitDataForForm(bool reapplyMods)
 }
 
 // Return true is the bought item has a max count to force refresh of window by caller
-bool Player::BuyItemFromVendorSlot(ObjectGuid vendorguid, uint32 item, uint8 count, uint8 bag, uint8 slot)
+bool Player::BuyItemFromVendorSlot(ObjectGuid vendorguid, uint32 /*vendorslot*/, uint32 item, uint8 count, uint8 bag, uint8 slot)
 {
     // cheating attempt
     if(count < 1) count = 1;

src/server/game/Entities/Player/Player.h
@@ -1387,7 +1387,8 @@ class TC_GAME_API Player : public Unit, public GridObject<Player>
             return !IsInFeralForm() && (!mainhand || !HasFlag(UNIT_FIELD_FLAGS,UNIT_FLAG_DISARMED) );
         }
         void SendNewItem( Item *item, uint32 count, bool received, bool created, bool broadcast = false, bool sendChatMessage = true);
-        bool BuyItemFromVendorSlot(ObjectGuid vendorguid, uint32 item, uint8 count, uint8 bag, uint8 slot);
+        //vendorslot unused on BC
+        bool BuyItemFromVendorSlot(ObjectGuid vendorguid, uint32 vendorslot, uint32 item, uint8 count, uint8 bag, uint8 slot);
 
         float GetReputationPriceDiscount( Creature const* pCreature ) const;
         float GetReputationPriceDiscount(FactionTemplateEntry const* factionTemplate) const;

src/server/game/Handlers/ItemHandler.cpp
@@ -695,9 +695,9 @@ void WorldSession::HandleBuyItemInSlotOpcode( WorldPacket & recvData )
 {
     ObjectGuid vendorguid, bagguid;
     uint32 item;
-    uint8 slot, count;
+    uint8 bagslot, count;
 
-    recvData >> vendorguid >> item >> bagguid >> slot >> count;
+    recvData >> vendorguid >> item >> bagguid >> bagslot >> count;
 
     uint8 bag = NULL_BAG;                                   // init for case invalid bagGUID
 
@@ -723,7 +723,7 @@ void WorldSession::HandleBuyItemInSlotOpcode( WorldPacket & recvData )
     if (bag == NULL_BAG)
         return;
 
-    GetPlayer()->BuyItemFromVendorSlot(vendorguid, item, count, bag,slot);
+    GetPlayer()->BuyItemFromVendorSlot(vendorguid, 0, item, count, bag, bagslot);
 }
 
 void WorldSession::HandleBuyItemOpcode( WorldPacket & recvData )
@@ -735,7 +735,7 @@ void WorldSession::HandleBuyItemOpcode( WorldPacket & recvData )
 
     recvData >> vendorguid >> item >> count >> unused;
 
-    GetPlayer()->BuyItemFromVendorSlot(vendorguid, item, count, NULL_BAG, NULL_SLOT);
+    GetPlayer()->BuyItemFromVendorSlot(vendorguid, 0, item, count, NULL_BAG, NULL_SLOT);
 }
 
 void WorldSession::HandleListInventoryOpcode(WorldPacket & recvData)
@@ -793,44 +793,47 @@ void WorldSession::SendListInventory(ObjectGuid vendorguid)
     {
         if(VendorItem const* crItem = vItems->GetItem(slot))
         {
-            if((crItem->proto->AllowableClass & _player->GetClassMask()) == 0 && crItem->proto->Bonding == BIND_WHEN_PICKED_UP && !_player->IsGameMaster())
-                continue;
+            if (ItemTemplate const* itemTemplate = sObjectMgr->GetItemTemplate(crItem->item))
+            {
+                if ((itemTemplate->AllowableClass & _player->GetClassMask()) == 0 && itemTemplate->Bonding == BIND_WHEN_PICKED_UP && !_player->IsGameMaster())
+                    continue;
 
 #ifdef LICH_KING
-            // Only display items in vendor lists for the team the
-            // player is on. If GM on, display all items.
-            if (!_player->IsGameMaster() && ((crItem->proto->Flags2 & ITEM_FLAGS_EXTRA_HORDE_ONLY && _player->GetTeamId() == ALLIANCE) || (crItem->proto->Flags2 == ITEM_FLAGS_EXTRA_ALLIANCE_ONLY && _player->GetTeamId() == HORDE)))
-                continue;
+                // Only display items in vendor lists for the team the
+                // player is on. If GM on, display all items.
+                if (!_player->IsGameMaster() && ((crItem->proto->Flags2 & ITEM_FLAGS_EXTRA_HORDE_ONLY && _player->GetTeamId() == ALLIANCE) || (crItem->proto->Flags2 == ITEM_FLAGS_EXTRA_ALLIANCE_ONLY && _player->GetTeamId() == HORDE)))
+                    continue;
 #endif
 
-            uint32 leftInStock = !crItem->maxcount ? 0xFFFFFFFF : pCreature->GetVendorItemCurrentCount(crItem);
-            /* Why should this be ?
-            // Items sold out are not displayed in list
-            if (!_player->IsGameMaster() && !leftInStock)
-                continue;
-            */
+                uint32 leftInStock = !crItem->maxcount ? 0xFFFFFFFF : pCreature->GetVendorItemCurrentCount(crItem);
+                /* Why should this be ?
+                // Items sold out are not displayed in list
+                if (!_player->IsGameMaster() && !leftInStock)
+                    continue;
+                */
 
-            if (!sConditionMgr->IsObjectMeetingVendorItemConditions(pCreature->GetEntry(), crItem->proto->ItemId, _player, pCreature))
-            {
-                TC_LOG_DEBUG("condition", "SendListInventory: conditions not met for creature entry %u item %u", pCreature->GetEntry(), crItem->proto->ItemId);
-                continue;
+                if (!sConditionMgr->IsObjectMeetingVendorItemConditions(pCreature->GetEntry(), crItem->item, _player, pCreature))
+                {
+                    TC_LOG_DEBUG("condition", "SendListInventory: conditions not met for creature entry %u item %u", pCreature->GetEntry(), crItem->item);
+                    continue;
+                }
+
+                // reputation discount
+                uint32 price = uint32(floor(itemTemplate->BuyPrice * discountMod));
+
+                data << uint32(slot + 1);  // client expects counting to start at 1
+                data << uint32(crItem->item);
+                data << uint32(itemTemplate->DisplayInfoID);
+                data << uint32(leftInStock);
+                data << uint32(price);
+                data << uint32(itemTemplate->MaxDurability);
+                data << uint32(itemTemplate->BuyCount);
+                data << uint32(crItem->ExtendedCost);
+
+                ++count;
+                if (count >= MAX_VENDOR_ITEMS)
+                    break;
             }
-
-            // reputation discount
-            uint32 price = uint32(floor(crItem->proto->BuyPrice * discountMod));
-
-            data << uint32(slot + 1);  // client expects counting to start at 1
-            data << uint32(crItem->proto->ItemId);
-            data << uint32(crItem->proto->DisplayInfoID);
-            data << uint32(leftInStock);
-            data << uint32(price);
-            data << uint32(crItem->proto->MaxDurability);
-            data << uint32(crItem->proto->BuyCount);
-            data << uint32(crItem->ExtendedCost);
-
-            ++count;
-            if (count >= MAX_VENDOR_ITEMS)
-                break;
         }
     }
 

TrinityTBC/core

commit e3d97da859dd512946d4eb56514351278b1955f2

Fixed vendors items from game events (was broken in 27d03800b0d40ea8f0b2d64c8b6a340c0c791a4e)

src/server/game/Globals/ObjectMgr.cpp
@@ -7580,10 +7580,10 @@ void ObjectMgr::LoadGossipMenuItems()
 void ObjectMgr::AddVendorItem( uint32 entry, ItemTemplate const *proto, uint32 maxcount, uint32 incrtime, uint32 extendedcost, bool savetodb)
 {
     VendorItemData& vList = m_mCacheVendorItemMap[entry];
-    vList.AddItem(entry, maxcount, incrtime, extendedcost);
+    vList.AddItem(proto->ItemId, maxcount, incrtime, extendedcost);
 
     if(savetodb) 
-        WorldDatabase.PExecute("INSERT INTO npc_vendor (entry,item,maxcount,incrtime,extendedcost) VALUES('%u','%u','%u','%u','%u')",entry, proto->ItemId, maxcount,incrtime,extendedcost);
+        WorldDatabase.PExecute("INSERT INTO npc_vendor (entry,item,maxcount,incrtime,extendedcost) VALUES('%u','%u','%u','%u','%u')", entry, proto->ItemId, maxcount,incrtime,extendedcost);
 }
 
 bool ObjectMgr::RemoveVendorItem( uint32 entry, ItemTemplate const *proto, bool savetodb)

src/server/game/Globals/ObjectMgr.h
@@ -1100,8 +1100,8 @@ class TC_GAME_API ObjectMgr
         {
             return Trinity::Containers::MapGetValuePtr(m_mCacheVendorItemMap, entry);
         }
-        void AddVendorItem(uint32 entry,ItemTemplate const *proto, uint32 maxcount, uint32 incrtime, uint32 ExtendedCost, bool savetodb = true); // for event
-        bool RemoveVendorItem(uint32 entry,ItemTemplate const *proto, bool savetodb = true); // for event
+        void AddVendorItem(uint32 entry,ItemTemplate const *proto, uint32 maxcount, uint32 incrtime, uint32 ExtendedCost, bool savetodb = false); // for event
+        bool RemoveVendorItem(uint32 entry,ItemTemplate const *proto, bool savetodb = false); // for event
         bool IsVendorItemValid( uint32 vendor_entry, ItemTemplate const *proto, uint32 maxcount, uint32 ptime, uint32 ExtendedCost, Player* pl = nullptr, std::set<uint32>* skip_vendors = nullptr, uint32 ORnpcflag = 0 ) const;
         bool AddTrainerSpell(uint32 creatureId, TrainerSpell const& spell);
         bool RemoveTrainerSpell(uint32 creatureId, uint32 spellId);
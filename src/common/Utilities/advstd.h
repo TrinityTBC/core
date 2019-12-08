a06c2140132aa98496f202e506616b9c182670f0
ryancheung <ryancheung.go@gmail.com>
2019-12-08 21:21:11 +0800

    Sync LambdaBasicEvent from TC

--- src/common/Utilities/advstd.h
+++ src/common/Utilities/advstd.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2018 TrinityCore <https://www.trinitycore.org/>
+ * Copyright (C) 2008-2019 TrinityCore <https://www.trinitycore.org/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -24,7 +24,7 @@
 #include <utility>
 
 // this namespace holds implementations of upcoming stdlib features that our c++ version doesn't have yet
-namespace advstd 
+namespace advstd
 {
     // C++17 std::apply (constrained to only function pointers, not all callable)
     template <typename... Ts>
@@ -73,20 +73,62 @@ namespace advstd
     // C++17 std::tuple_size_v
     forward_1v(tuple_size, size_t);
 
-	 // C++17 std::is_enum_v
+    // C++17 std::is_enum_v
     forward_1v(is_enum, bool);
-     // C++17 std::is_arithmetic_v
+
+    // C++17 std::is_arithmetic_v
     forward_1v(is_arithmetic, bool);
-	
+
+    // C++17 std::is_move_assignable_v
+    forward_1v(is_move_assignable, bool);
+
 #undef forward_1v
 #undef forward_2v
 
     // C++17 std::size
-    template <class C>
-    constexpr auto size(const C& c) -> decltype(c.size()) { return c.size(); }
+    template <typename C>
+    constexpr auto size(const C& c) { return c.size(); }
 
-    template <class T, std::size_t N>
-    constexpr std::size_t size(const T(&array)[N]) noexcept { return N; }
+    template <typename T, std::size_t N>
+    constexpr std::size_t size(const T(&)[N]) noexcept { return N; }
+
+    // C++17 std::data
+    template <typename C>
+    constexpr auto data(C& c) { return c.data(); }
+
+    template <typename C>
+    constexpr auto data(C const& c) { return c.data(); }
+
+    template <typename T, std::size_t N>
+    constexpr T* data(T(&a)[N]) noexcept { return a; }
+
+    template <typename T, std::size_t N>
+    constexpr T const* data(const T(&a)[N]) noexcept { return a; }
+
+    template <typename T>
+    constexpr T const* data(std::initializer_list<T> l) noexcept { return l.begin(); }
+
+    // C++17 std::gcd
+    template <typename T1, typename T2>
+    constexpr std::enable_if_t<advstd::is_unsigned_v<T1> && advstd::is_unsigned_v<T2>, std::common_type_t<T1, T2>> gcd(T1 _m, T2 _n)
+    {
+        using T = std::common_type_t<T1, T2>;
+        T n=_n, m=_m;
+        while (n)
+        {
+            T o = m;
+            m = n;
+            n = o%n;
+        }
+        return m;
+    }
+
+    // C++17 std::lcm
+    template <typename T1, typename T2>
+    constexpr std::enable_if_t<advstd::is_unsigned_v<T1> && advstd::is_unsigned_v<T2>, std::common_type_t<T1, T2>> lcm(T1 m, T2 n)
+    {
+        return (m/gcd(m, n))*n;
+    }
 
     // C++20 std::remove_cvref_t
     template <class T>

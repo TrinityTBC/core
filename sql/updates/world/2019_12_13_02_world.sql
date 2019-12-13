ed83faa00900f61a3ef755c2b6ed67048cf110ca
ryancheung <ryancheung.go@gmail.com>
2019-12-13 16:50:06 +0800

    Fix few missing areatrigger_scripts data and the ScriptName of item#19161

---
+++ sql/updates/world/2019_12_13_02_world.sql
@@ -0,0 +1,29 @@
+--
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (4017, 'at_twilight_grove');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (4016, 'at_malfurion_stormrage');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (4752, 'at_nats_landing');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (3551, 'at_bring_your_orphan_to');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (3549, 'at_bring_your_orphan_to');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (3547, 'at_bring_your_orphan_to');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (3546, 'at_bring_your_orphan_to');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (3552, 'at_bring_your_orphan_to');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (3548, 'at_bring_your_orphan_to');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (4356, 'at_bring_your_orphan_to');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (4829, 'at_brewfest');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (4820, 'at_brewfest');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (3587, 'at_ancient_leaf');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (2046, 'at_dragonspire_hall');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (2026, 'at_blackrock_stadium');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (4524, 'at_nethekurse_exit');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (5279, 'areatrigger_orgrimmar_herald_of_war');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (1946, 'at_nearby_scarshield_infiltrator');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (4665, 'at_teron_gorefiend_entrance');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (302, 'at_sentry_point');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (2066, 'at_trigger_the_beast_movement');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (2067, 'at_the_beast_room');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (4052, 'at_aq_battleguard_sartura');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (3957, 'at_zulgurub_entrance');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (3958, 'at_zulgurub_entrance');
+REPLACE INTO `areatrigger_scripts`(`entry`, `ScriptName`) VALUES (3960, 'at_zulgurub_entrance');
+
+UPDATE `item_template` SET `ScriptName`='item_generic_limit_chance_above_60' WHERE `entry`=19169;

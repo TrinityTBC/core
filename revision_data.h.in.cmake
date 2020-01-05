e61cbfff9cc1dfa6adfe6bcfc8294b16a98a7a04
ryancheung <ryancheung.go@gmail.com>
2020-01-05 18:40:30 +0800

    Hackfix for the weird msvc 4005 macro redefinition warnings in revision_data.h header, maybe it was caused by using cmake 3.16 pch?

--- revision_data.h.in.cmake
+++ revision_data.h.in.cmake
@@ -10,10 +10,16 @@
  #define _BUILD_DIRECTORY           R"(@BUILDDIR@)"
  #define _MYSQL_EXECUTABLE          R"(@MYSQL_EXECUTABLE@)"
  #define _FULL_DATABASE             "TTDB_full_world_19124_2019_12_22.sql"
+ #ifndef VER_COMPANYNAME_STR
  #define VER_COMPANYNAME_STR        "TrinityCore Developers"
+ #endif
  #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2019 TrinityCore"
  #define VER_FILEVERSION            0,0,0
  #define VER_FILEVERSION_STR        "@rev_hash@ @rev_date@ (@rev_branch@ branch)"
+ #ifndef VER_PRODUCTVERSION
  #define VER_PRODUCTVERSION         VER_FILEVERSION
+ #endif
+ #ifndef VER_PRODUCTVERSION_STR
  #define VER_PRODUCTVERSION_STR     VER_FILEVERSION_STR
+ #endif
 #endif // __REVISION_DATA_H__

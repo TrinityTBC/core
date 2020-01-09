81e613f3b0bbab731e0dcbcafaf42013e48c236f
ryancheung <ryancheung.go@gmail.com>
2020-01-09 12:49:10 +0800

    DB: Fix few item with different data than dbc

---
+++ sql/updates/world/2020_01_09_00_world.sql
@@ -0,0 +1,5 @@
+--
+UPDATE `item_template` SET `sheath`=0 WHERE `entry`=5259;
+UPDATE `item_template` SET `displayid`=36036 WHERE `entry`=24418;
+UPDATE `item_template` SET `displayid`=37829 WHERE `entry`=25818;
+UPDATE `item_template` SET `displayid`=31265 WHERE `entry`=29455;
